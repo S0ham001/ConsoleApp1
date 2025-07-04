6968a68b881ffeb36b5cb5267d9badee41a50f18
COCO <sthummar444@rku.ac.in>
2025-07-04 11:06:06 +0530

    test 2

--- Bhoomi_maam.cs
+++ Bhoomi_maam.cs
@@ -10,13 +10,22 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-            Console.Write("enter 1st number: ");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("enter 2nd number: ");
-            int b = Convert.ToInt32(Console.ReadLine());
-            int sum = a + b;
-            Console.WriteLine($"sum is {a} + {b} = " + sum);
-            Console.Read();
+            //T1.1
+            //Console.WriteLine("Hellow");
+
+
+            //T1.2
+            Console.WriteLine("Name: Soham");
+            Console.WriteLine("DOB: 29/02/2004");
+            Console.WriteLine("Address: balaji_complex, vadal_main_road,tramba");
+            Console.WriteLine("City:Rajkot");
+            Console.WriteLine("Pin code: 360020");
+            Console.WriteLine("State: Gujrat");
+            Console.WriteLine("Contry: India");
+            Console.WriteLine("Email:sthummar444@rku.ac.in");
+
+            //T1.3
+
         }
 
     }
