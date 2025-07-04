cc0de962541fc6c026d9625d8bf8f047183e592c
COCO <sthummar444@rku.ac.in>
2025-07-04 10:36:03 +0530

    test

---
+++ Bhoomi_maam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Bhoomi_maam
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("enter 1st number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter 2nd number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = a + b;
+            Console.WriteLine($"sum is {a} + {b} = " + sum);
+            Console.Read();
+        }
+
+    }
+}
