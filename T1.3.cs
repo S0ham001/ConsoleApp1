a280d77d5154cdebc479f006b4b42681a6ad2358
COCO <sthummar444@rku.ac.in>
2025-07-26 10:48:21 +0530

    29/07/2025

---
+++ T1.3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ConsoleApp1
+
+{
+    internal class Program3
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(number + " is Even.");
+            }
+            else
+            {
+                Console.WriteLine(number + " is Odd.");
+            }
+        }
+    }
+}
+
+
