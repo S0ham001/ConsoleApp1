66acee60f050bb5677540e198bf2f76979dbfe9c
COCO <sthummar444@rku.ac.in>
2025-08-26 06:14:48 +0530

    emp.cs = practice static keyword bu nirav sir

---
+++ emp.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class emp
+    {
+        public class Employee
+        {
+            private int empid;
+            private string name;
+            private string designation;
+            private string instname;
+            private double basic_Pay;
+            public int Empid
+            {
+                get { return empid; }
+                set { empid = value; }
+            }
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public string Designation
+            {
+                get { return designation; }
+                set { designation = value; }
+            }
+            public string Instname
+            {
+                get { return instname; }
+                set { instname = value; }
+            }
+            public double basicPay
+            {
+                get { return basic_Pay; }
+                set { basic_Pay = value; }
+            }
+            public void display()
+            {
+                Console.WriteLine("Employee ID: " + empid);
+                Console.WriteLine("Employee Name: " + name);
+                Console.WriteLine("Employee Designation: " + designation);
+                Console.WriteLine("Employee Instname: " + instname);
+                Console.WriteLine("Employee basic pay: " + basic_Pay);
+            }
+            public static double calculateSalary(double basicPay)
+            {
+                return basicPay + (basicPay * 0.1) + (basicPay * 0.45);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.Empid = 101;
+            emp1.Name = "Ron";
+            emp1.basicPay = 25000;
+            emp1.Designation = "Developer";
+            emp1.Instname = "RK University";
+            double emp1salry = Employee.calculateSalary(emp1.basicPay);
+
+            Employee emp2 = new Employee();
+            emp2.Empid = 102;
+            emp2.Name = "Harry";
+            emp2.basicPay = 30000;
+            emp2.Designation = "Manager";
+            emp2.Instname = "RK University";
+            double emp2salry = Employee.calculateSalary(emp2.basicPay);
+
+            if (emp2salry > emp1salry)
+            {
+                emp2.display();
+                Console.WriteLine("Employee Salary: " + emp2salry);
+            }
+            else
+            {
+                emp1.display();
+                Console.WriteLine("Employee Salary: " + emp1salry);
+            }
+
+        }
+    }
+
+}
---
+++ static_keyword.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Demo2
+{
+    int x;
+    static int y = 20;
+
+    public Demo2(int x)
+        {
+            this.x = x;
+        }
+
+    public static int addNum()
+    {
+        Demo2 ob = new Demo2(30);
+        return (ob.x + y);
+    }
+
+    public void show()
+    {
+        Console.WriteLine("X is :" + x);
+        Console.WriteLine("Y is :" + y);
+    }
+
+}
+    public static class StaticClass
+    {
+        // only allow static members,method and constructors
+
+        static int x;
+
+        static StaticClass()
+        {
+            x = 100;
+        }
+        public static void display()
+        {
+            Console.WriteLine("x is :" + x);
+        }
+    }
+
+    internal class statickeyword
+{
+    public static void Main(string[] args)
+    {
+        Demo2 ob = new Demo2(30);
+        
+        ob.show();
+            //to call static method use <<classname>>.<<methodname>>
+            Console.WriteLine(Demo2.addNum());
+            //we dont't need to create object of a static class
+            StaticClass.display();
+        }
+}
+}
